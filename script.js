217ad8233ea95f1befbb81bc60e19138e36cd515
Maksym <88537213+DMV22@users.noreply.github.com>
2023-11-16 23:48:35 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function changeFlag() {
-  var select = document.getElementById("country");
-  var selectedFlag = document.getElementById("selected-flag");
-  var selectedOption = select.options[select.selectedIndex];
-  var flag = selectedOption.getAttribute("data-flag");
-  selectedFlag.textContent = flag;
-}
