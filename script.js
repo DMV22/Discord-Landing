a0091f31428fda835f2df0d6d8a7f3eaa59c34b7
DMV22 <maksdivnich12@gmail.com>
2023-11-19 02:41:40 +0200

    adding burger-menu and fixed header

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-function changeFlag() {
-  var select = document.getElementById("country");
-  var selectedFlag = document.getElementById("selected-flag");
-  var selectedOption = select.options[select.selectedIndex];
-  var flag = selectedOption.getAttribute("data-flag");
-  selectedFlag.textContent = flag;
+const openButton = document.querySelector(".header__menu-btn");
+const sidebar = document.querySelector(".sidebar-container");
+const closeButton = document.querySelector(".close");
+
+const modalOverlay = document.querySelector(".modal__overlay");
+const body = document.querySelector("body");
+
+function openSidebar() {
+  sidebar.classList.add("active");
+  modalOverlay.classList.add("visible");
+  body.classList.add("hidden");
 }
+openButton.addEventListener("click", openSidebar);
+
+function closeSidebar() {
+  sidebar.classList.remove("active");
+  modalOverlay.classList.remove("visible");
+  body.classList.remove("hidden");
+}
+closeButton.addEventListener("click", closeSidebar);
+modalOverlay.addEventListener("click", closeSidebar);
